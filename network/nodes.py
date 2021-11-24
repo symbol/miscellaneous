b417ed9fa9b379f7863f6a381f62b1a479228359
Jaguar0625 <jaguar0625@127.0.0.1>
2021-11-24 08:26:40 -0400

    task: add network filter when doing crawl

--- network/nodes.py
+++ network/nodes.py
@@ -17,10 +17,13 @@ from client.ResourceLoader import load_resources, locate_blockchain_client_class
 class NodeDownloader:
     # pylint: disable=too-many-instance-attributes
 
-    def __init__(self, resources, thread_count, timeout, certificate_directory):
+    def __init__(self, resources, thread_count, timeout, network_name, certificate_directory):
+        # pylint: disable=too-many-arguments
+
         self.resources = resources
         self.thread_count = thread_count
         self.timeout = timeout
+        self.network_name = network_name
         self.certificate_directory = certificate_directory
 
         self.api_client_class = locate_blockchain_client_class(resources)
@@ -75,47 +78,77 @@ class NodeDownloader:
                 json_node = api_client.get_node_info()
                 json_node['extraData'] = {'balance': 0, 'height': 0, 'finalizedHeight': 0}
 
-                strong_api_client = random.choice(self.strong_api_clients)
-                if self.is_nem:
-                    network = NetworkLocator.find_by_identifier(NemNetwork.NETWORKS, json_node['metaData']['networkId'])
-                    node_address = network.public_key_to_address(PublicKey(json_node['identity']['public-key']))
-                    main_account_info = strong_api_client.get_account_info(node_address, forwarded=True)
+                network = self._get_and_check_network(json_node)
+                if network:
+                    main_public_key = self._find_main_public_key(network, json_node)
+                    is_reachable = True
 
-                    if 'ACTIVE' == main_account_info.remote_status:
-                        json_node['identity']['node-public-key'] = json_node['identity']['public-key']
-                        json_node['identity']['public-key'] = main_account_info.public_key
+                    json_peers = api_client.get_peers()
 
-                    main_public_key = PublicKey(json_node['identity']['public-key'])
-                else:
-                    network = NetworkLocator.find_by_identifier(SymbolNetwork.NETWORKS, json_node['networkIdentifier'])
-
-                    main_public_key = PublicKey(json_node['publicKey'])
-
-                is_reachable = True
-
-                json_peers = api_client.get_peers()
-
-                main_account_info = strong_api_client.get_account_info(network.public_key_to_address(main_public_key))
-
-                json_node['extraData']['balance'] = main_account_info.balance if main_account_info else 0
-                json_node['extraData']['height'] = api_client.get_chain_height()
-
-                if not self.is_nem:
-                    json_node['extraData']['finalizedHeight'] = api_client.get_finalization_info().height
+                    self._add_supplemental_account_information(network, main_public_key, json_node, api_client)
 
             except (RequestException, TimeoutError, ConnectionRefusedError) as ex:
                 log.warning(f'failed to load peers from {api_client.node_host}:{api_client.node_port} (reachable node? {is_reachable})\n'
                             f'{ex}')
                 json_peers = []
 
-            with self.lock:
-                if is_reachable:
-                    self._update(main_public_key, json_node, json_peers)
+            finally:
+                with self.lock:
+                    if is_reachable:
+                        self._update(main_public_key, json_node, json_peers)
 
-                self.busy_thread_count -= 1
+                    self.busy_thread_count -= 1
 
-            if self.busy_thread_count < self.thread_count - 1:
-                log.debug(f'idling threads detected; only {self.busy_thread_count} busy')
+                if self.busy_thread_count < self.thread_count - 1:
+                    log.debug(f'idling threads detected; only {self.busy_thread_count} busy')
+
+    def _get_and_check_network(self, json_node):
+        if self.is_nem:
+            networks = NemNetwork.NETWORKS
+            network_identifier = json_node['metaData']['networkId'] & 0xFF  # coerce to unsigned
+        else:
+            networks = SymbolNetwork.NETWORKS
+            network_identifier = json_node['networkIdentifier']
+
+        try:
+            network = NetworkLocator.find_by_identifier(networks, network_identifier)
+        except StopIteration:
+            log.warning(f'rejecting node from network with unknown identifier {network_identifier}')
+            return None
+
+        expected_network = NetworkLocator.find_by_name(networks, self.network_name)
+
+        if network.identifier == expected_network.identifier:
+            return network
+
+        log.warning(f'rejecting node from network {network.name} when looking for nodes from {self.network_name}')
+        return None
+
+    def _find_main_public_key(self, network, json_node):
+        strong_api_client = random.choice(self.strong_api_clients)
+
+        if self.is_nem:
+            node_address = network.public_key_to_address(PublicKey(json_node['identity']['public-key']))
+            main_account_info = strong_api_client.get_account_info(node_address, forwarded=True)
+
+            if 'ACTIVE' == main_account_info.remote_status:
+                json_node['identity']['node-public-key'] = json_node['identity']['public-key']
+                json_node['identity']['public-key'] = main_account_info.public_key
+
+            return PublicKey(json_node['identity']['public-key'])
+
+        return PublicKey(json_node['publicKey'])
+
+    def _add_supplemental_account_information(self, network, main_public_key, json_node, api_client):
+        strong_api_client = random.choice(self.strong_api_clients)
+
+        main_account_info = strong_api_client.get_account_info(network.public_key_to_address(main_public_key))
+
+        json_node['extraData']['balance'] = main_account_info.balance if main_account_info else 0
+        json_node['extraData']['height'] = api_client.get_chain_height()
+
+        if not self.is_nem:
+            json_node['extraData']['finalizedHeight'] = api_client.get_finalization_info().height
 
     # this function must be called in context of self.lock
     def _pop_next_api_client(self):
@@ -171,11 +204,12 @@ def main():
     parser.add_argument('--output', help='output file', required=True)
     parser.add_argument('--thread-count', help='number of threads', type=int, default=16)
     parser.add_argument('--timeout', help='peer timeout', type=int, default=20)
+    parser.add_argument('--network', help='network name filter', default='mainnet')
     parser.add_argument('--certs', help='ssl certificate directory (required for Symbol peer node communication)')
     args = parser.parse_args()
 
     resources = load_resources(args.resources)
-    downloader = NodeDownloader(resources, args.thread_count, args.timeout, args.certs)
+    downloader = NodeDownloader(resources, args.thread_count, args.timeout, args.network, args.certs)
     downloader.discover()
     downloader.save(args.output)
 
