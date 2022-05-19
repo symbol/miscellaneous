479d6b4b42fb883f383d2abafd28778e5eb4767a
gimre <g.imre@protonmail.com>
2022-05-19 16:20:20 +0200

    disable similarities checker under block dir

--- lint.sh
+++ lint.sh
@@ -5,4 +5,5 @@ set -ex
 find . -type f -name "*.sh" -print0 | xargs -0 shellcheck
 find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m isort --check-only --line-length 140
 find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pycodestyle --config=.pycodestyle
-find . -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pylint --load-plugins pylint_quotes
+find . -type f -path ./block -prune -o -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pylint --load-plugins pylint_quotes
+find block -type f -name "*.py" -print0 | PYTHONPATH=. xargs -0 python3 -m pylint --load-plugins pylint_quotes --disable=similarities
