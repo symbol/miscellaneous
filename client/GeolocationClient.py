a28f3d195a76402b07eacb4951549a417a5bdda7
Anthony Law <anthony@symbol.dev>
2023-03-08 15:20:13 +0800

    [network] feature: add geolocation related code in client and network #31

---
+++ client/GeolocationClient.py
@@ -0,0 +1,13 @@
+from .TimeoutHTTPAdapter import create_http_session
+
+class GeolocationClient:
+    def __init__(self):
+        self.session = create_http_session()
+
+    def get_ip_geolocation(self, ips):
+        json_http_headers = {'Content-type': 'application/json'}
+        json_response = self.session.post(
+            f'http://ip-api.com/batch?fields=1057493',
+            json=ips,
+            headers=json_http_headers).json()
+        return json_response
