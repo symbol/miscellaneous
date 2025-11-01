1991f941bd0545b6850118baa0395917a787b9ba
ccHarvestasya <harvestasya84@gmail.com>
2025-11-01 18:32:03 +0900

    fix: linting errors

--- network/richlist_symbol.py
+++ network/richlist_symbol.py
@@ -18,14 +18,13 @@ class RichListDownloader:
 		self.min_balance = min_balance
 		self.mosaic_id = mosaic_id
 		self.nodes_input_filepath = nodes_input_filepath
-		self.timeout = timeout
 
-		self.api_client_kwargs = {
-			'timeout': self.timeout,
+		api_client_kwargs = {
+			'timeout': timeout,
 			'retry_count': 2,
 			'retry_post': False
 		}
-		self.api_client = create_blockchain_api_client(self.resources, **self.api_client_kwargs)
+		self.api_client = create_blockchain_api_client(self.resources, **api_client_kwargs)
 
 		self.finalization_epoch = 0
 		self.voters_map = {}
